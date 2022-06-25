3c0b571b586a9fb87ecc71698e235465d6b60761
Abraham Adeleke <abrahamadeleke00@gmail.com>
2022-06-25 10:36:29 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+word1 = input("what is your sentence?: ")
+word2 = input("what word do you want to replace?: ")
+word3 = input(" what word do you want to replace the word with?: ")
+new_word = word1.replace(word2,word3)
+print(" your new word is " + new_word)
